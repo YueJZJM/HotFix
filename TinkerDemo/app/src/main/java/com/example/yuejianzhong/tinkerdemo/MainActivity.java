1b1baa436daf4faf9c77ad55e7416bc820b9a9ae
岳剑钟 <yuejzhfut@163.com>
2019-04-18 23:20:00 +0800

    tinker的 gradle 集成

--- TinkerDemo/app/src/main/java/com/example/yuejianzhong/tinkerdemo/MainActivity.java
+++ TinkerDemo/app/src/main/java/com/example/yuejianzhong/tinkerdemo/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.yuejianzhong.tinkerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.yuejianzhong.tinkerdemo.tinker.TinkerManager;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILE_END = ".apk";
+    private String mPatchDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPatchDir = getExternalCacheDir().getAbsolutePath() + "/tpatch/";
+        Log.d("yuejz", mPatchDir);
+        File file = new File(mPatchDir);
+        if (file == null || file.exists()) {
+            file.mkdir();
+        }
+    }
+
+    public void loadPatch(View view) {
+        TinkerManager.loadPatch(getPatchName());
+    }
+
+    private String getPatchName() {
+        return mPatchDir.concat("imooc").concat(FILE_END);
     }
 }
